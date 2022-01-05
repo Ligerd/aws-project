4e86b667a13b4df8ad63fb51adb186064f95b4e3
Ligerd <fa_74@list.ru>
2022-01-05 18:12:13 +0100

    Preparation before PR

--- src/db/sql/insert_test_data.sql
+++ src/db/sql/insert_test_data.sql
@@ -16,19 +16,19 @@ VALUES ('Test1', 'Test1', 'Test_location', '57324234'),
        ('Test2', 'Test3', 'Test_location2', '77324234');
 
 
-INSERT INTO public."OrderDetails" ("orderDate", "shipmentDate","shipmentStatus")
+INSERT INTO "OrderDetails" ("orderDate", "shipmentDate","shipmentStatus")
 VALUES ('2021-12-6 19:10:25-07', '2022-01-6 19:10:25-07','Preparation'),
        ('2021-11-6 19:10:25-07', '2022-12-6 19:10:25-07','Delivered'),
        ('2021-12-9 19:10:25-07', '2022-01-31 19:10:25-07','On the way');
 
 
-INSERT INTO public."Orders" ("customerId", "orderedProducts", "totalPrice")
+INSERT INTO "Orders" ("customerId", "orderedProducts", "totalPrice")
 VALUES (1, '{1,2}', 11000),
        (2, '{7,8,4}', 170000),
        (3, '{5,5}', 20000);
 
 
-INSERT INTO public."Delivery" ("orderId", city, "postCode",street,"houseNumber")
+INSERT INTO "Delivery" ("orderId", city, "postCode",street,"houseNumber")
 VALUES (1, 'Rome', '12103','Kaiserin-Augusta-Straße',7),
        (2, 'Rome', '00182','Via Noto',35),
        (3, 'Moscow', '115280','ул. Архитектора Щусева',12);
