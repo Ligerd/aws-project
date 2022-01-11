ff4d116493e33e9ad972c709502e53a788835896
Jakub Korczakowski <jakubkorczakowski@gmail.com>
2022-01-11 18:31:34 +0100

    Fix password situation.

--- src/orders/app/database.py
+++ src/orders/app/database.py
@@ -3,8 +3,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 
 from sqlalchemy.orm import sessionmaker
+import os
 
-SQLALCHEMY_DATABASE_URL = f"postgresql://webshopadmin@webshopsuper:s2pgR5tSbe9UqtcR@webshopsuper.postgres.database.azure.com:5432/online_shope"
+SQLALCHEMY_DATABASE_URL = f"postgresql://webshopadmin@webshopsuper:{os.getenv('POSTGRES_PASSWORD')}@webshopsuper.postgres.database.azure.com:5432/online_shope"
 engine = create_engine(
     # SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
     SQLALCHEMY_DATABASE_URL, connect_args={'sslmode':'require'}
